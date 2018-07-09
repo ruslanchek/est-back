39f7c01e82479d987b47606f5feb6dbda4ae1408
Ruslan <ruslanchek@gmail.com>
2018-07-09 16:55:21 +0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { ApplicationModule } from './app.module';
 import { swagger } from './swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule);
+  const app = await NestFactory.create(ApplicationModule, {
+    cors: true
+  });
+
   swagger(app);
+  
   await app.listen(5666);
 }
 
