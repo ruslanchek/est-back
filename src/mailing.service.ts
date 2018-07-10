a7752c9a5dcdc6575c7a0a435664a0dcd4e0a9cf
Ruslan <ruslanchek@gmail.com>
2018-07-10 19:16:34 +0300

    Mailer

--- src/mailing.service.ts
+++ src/mailing.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { MailOptions } from 'nodemailer/lib/smtp-transport';
 
 @Injectable()
 export class MailingService {
-    public send(): string {
-        
+	private transport: nodemailer.Transporter = null;
 
-        return 'sent';
-    }
+	constructor() {
+		this.transport = nodemailer.createTransport({
+			service: 'Gmail',
+			auth: {
+				user: 'ruslanchek@gmail.com',
+				pass: 'gxgkyndjizgnjdah'
+			}
+		});
+	}
+
+	private createMailOptions(): MailOptions {
+		return {
+			from: 'Ruslan Shashkov ✔ <ruslanchek@gmail.com>',
+			to: 'rshashkov@icloud.com',
+			subject: 'Hello ✔',
+			text: 'Hello world ✔',
+			html: '<b>Hello world ✔</b>'
+		};
+	}
+
+	public send(): void {
+		this.transport.sendMail(this.createMailOptions(), (error, response) => {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log("Message sent: " + response.message);
+			}
+		});
+	}
 }
