21b831df3c6050519c68b7ff46516a524a1d398d
ruslan <ruslanchek@gmail.com>
2018-07-10 09:26:09 +0300

    Update!

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,10 +1,9 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { Injectable, HttpStatus } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AUTH_POLICY } from './auth.policy';
 import { IJwtPayload } from './auth.interface';
-import { Api, EApiErrorCode } from 'api';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -21,9 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       done(null, user);
     } catch (e) {
       return done(
-        Api.error(HttpStatus.UNAUTHORIZED, {
-          code: EApiErrorCode.UNAUTHORIZED,
-        }),
+        new UnauthorizedException(),
         false,
       );
     }
