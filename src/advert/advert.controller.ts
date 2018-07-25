86a62dc98bf99f62e0cc0ab848ddec28130743e0
Ruslan Shashkov <ruslanchek@icloud.com>
2018-07-26 01:23:17 +0300

    ENV

--- src/advert/advert.controller.ts
+++ src/advert/advert.controller.ts
@@ -1,10 +1,25 @@
-import { Get, Controller, Query, Post, HttpStatus, HttpException, Body, Patch, Put, Param, UseGuards, Request } from '@nestjs/common';
+import {
+  Get,
+  Controller,
+  Query,
+  Post,
+  HttpStatus,
+  HttpException,
+  Body,
+  Patch,
+  Put,
+  Param,
+  UseGuards,
+  Request,
+  UseInterceptors, FileInterceptor, UploadedFile,
+} from '@nestjs/common';
 import { AdvertService } from './advert.service';
 import { Advert } from './advert.entity';
 import { CreateAdvertDto, UpdateAdvertDto } from './advert.dto';
 import { ValidationPipe } from '../validation.pipe';
 import { Api, IApiResult, IApiResultCreate, IApiResultList, IApiResultOne } from '../api';
 import { AuthGuard } from '@nestjs/passport';
+import { IFile } from '../upload.service';
 
 @Controller('/api/advert')
 export class AdvertController {
@@ -36,4 +51,16 @@ export class AdvertController {
 
     return await this.advertService.update(user.id, params.id, dto);
   }
+
+  // @Post('add-images')
+  // @UseGuards(AuthGuard('jwt'))
+  // @UseInterceptors(FileInterceptor('file'))
+  // async updateAvatar(
+  //   @Request() req,
+  //   @UploadedFile() file: IFile,
+  // ) {
+  //   const { user } = req;
+  //
+  //   return await this.profileService.updateAvatar(user.id, file);
+  // }
 }
